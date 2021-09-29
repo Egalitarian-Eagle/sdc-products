bfcd758060681c7a91e2491795145999ad947845
alan_fong <alanfong@berkeley.edu>
2021-09-29 15:29:54 -0700

    Finished most basic ETL for schema
    Added csv folder and added to gitignore

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-DROP DATABASE test;
+-- DROP DATABASE test;
 CREATE DATABASE test;
 
 CREATE TABLE products (
@@ -11,38 +11,46 @@ CREATE TABLE products (
 );
 
 CREATE TABLE features (
+  feature_id integer PRIMARY KEY,
+  product_id integer REFERENCES products(product_id),
   feature varchar(80),
-  value varchar(80),
-  product_id integer REFERENCES products(product_id)
+  value varchar(80)
 );
 
 -- isDefault in the old api is referenced as 'default?'
 CREATE TABLE styles (
-  product_id integer REFERENCES products (product_id),
   style_id integer PRIMARY KEY,
+  product_id integer REFERENCES products (product_id),
   name varchar(80),
-  original_price varchar(10),
   sale_price varchar(10),
+  original_price varchar(10),
   isDefault boolean
 );
 
 
 CREATE TABLE photos (
+  id integer PRIMARY KEY,
   style_id integer REFERENCES styles (style_id),
   thumbnail_url TEXT,
   url TEXT
 );
 
 CREATE TABLE skus (
-  style_id integer REFERENCES styles (style_id),
   sku_id integer PRIMARY KEY,
-  quantity integer,
-  size varchar(5)
+  style_id integer REFERENCES styles (style_id),
+  size varchar(10),
+  quantity integer
 );
 
 CREATE TABLE related (
+  id integer PRIMARY KEY,
   product_id integer REFERENCES products(product_id),
   realted_id integer
 );
 
-COPY products FROM '/home/alan_fong/hackreactor/SDC/sdc-products/product.csv' CSV HEADER;
+COPY products FROM '/home/alan_fong/hackreactor/SDC/sdc-products/csv/product.csv' CSV HEADER;
+COPY features FROM '/home/alan_fong/hackreactor/SDC/sdc-products/csv/features.csv' CSV HEADER;
+COPY styles FROM '/home/alan_fong/hackreactor/SDC/sdc-products/csv/styles.csv' CSV HEADER;
+COPY photos FROM '/home/alan_fong/hackreactor/SDC/sdc-products/csv/photos.csv' CSV HEADER;
+COPY skus FROM '/home/alan_fong/hackreactor/SDC/sdc-products/csv/skus.csv' CSV HEADER;
+COPY related FROM '/home/alan_fong/hackreactor/SDC/sdc-products/csv/related.csv' CSV HEADER;
