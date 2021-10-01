fbf582a58c723a62712f2aac5548b52541347bad
alan_fong <alanfong@berkeley.edu>
2021-09-30 23:09:09 -0700

    Finished template for queries
    Need to make queries dynamic

--- newServer/models/models.js
+++ newServer/models/models.js
@@ -20,19 +20,71 @@ module.exports = {
     })
   },
 
-  getStyles: (callback) => {
-    pool.query('SELECT * FROM styles WHERE product_id=1', (error, data) => {
-      if (error) {
-        callback(error, null);
-      } else {
-        const information = {};
-        information.results = data.rows;
-        // Add product info
-        // Add photos for each of the results
-        callback(null, information);
-      }
-    })
+  // getStyles: (callback) => {
+  //   pool.query('SELECT * FROM styles WHERE product_id=1', async (error, data) => {
+  //     if (error) {
+  //       callback(error, null);
+  //     } else {
+  //       const information = {};
+  //       information.results = data.rows;
+  //       // Add product info
+  //       // Add photos for each of the results
+  //       await information.results.forEach(async (result) => {
+  //         result.photos = await pool.query(`SELECT * FROM photos WHERE style_id=${result.style_id}`, (error, data) => {
+  //           if(error) {
+  //             console.log(error);
+  //           } else {
+  //             return data;
+  //           }
+  //         });
+  //       })
+  //       callback(null, information);
+  //     }
+  //   })
+  // },
+  getStyles: async (callback) => {
+    const information = {};
+    await pool.query('SELECT * FROM styles WHERE product_id=1')
+      .then((response) => {
+        information.results = response.rows;
+      })
+    await Promise.all(information.results.map(async (result) => {
+      await pool.query(`SELECT * FROM photos WHERE style_id=${result.style_id}`)
+        .then((response) => {
+          result.photos = response.rows;
+          result.skus = {};
+        })
+    }))
+    await Promise.all(information.results.map(async (result) => {
+      await pool.query(`SELECT * FROM skus WHERE style_id=${result.style_id}`)
+        .then((response) => {
+          // console.log(response.rows)
+          response.rows.forEach((sku) => {
+            const id = sku.sku_id;
+            // skuData[id] = sku;
+            result.skus[id] = sku;
+          })
+        })
+    }))
+    callback(null, information);
+      // .then(async (data) => {
+      //   console.log(data);
+      //   await data.results.forEach(async (result) => {
+      //     await pool.query(`SELECT * FROM photos WHERE style_id=${result.style_id}`)
+      //       .then((response) => {
+      //         result.photos = response.rows;
+      //       })
+      //   })
+      //   callback(null, data);
+      // })
+      // .then((response) => {
+      //   console.log(response);
+      // })
   },
+
+  // getPhotos: (callback) => {
+  //   pool.query('SELECT * FROM photos WHERE style_id=1')
+  // }
 
   getRelated: (callback) => {
     pool.query('SELECT * FROM related WHERE product_id=1', (error, data) => {
