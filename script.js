211cef5e761dfb3e2e1c4a9a8e4574559e152975
alan_fong <alanfong@berkeley.edu>
2021-10-04 10:42:37 -0700

    Refactored test to stress test based on objectives

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+
 export let options = {
-  vus: 1000,
-  duration: '30s',
-  // stages: [
-  //   { duration: '30s', target: 20 },
-  //   { duration: '1m30s', target: 10 },
-  //   { duration: '20s', target: 0 },
-  // ],
-}
+  stages: [
+    { duration: '30s', target: 1 },
+    { duration: '30s', target: 100 },
+    { duration: '30s', target: 100 },
+    { duration: '30s', target: 1000 },
+    { duration: '1m', target: 1000 },
+    { duration: '30s', target: 0 },
+  ],
+};
 
 export default function () {
-  const productId = Math.floor(Math.random() * 1000000)
-  http.get(`http://localhost:3000/products/${productId}`);
-  http.get(`http://localhost:3000/products/${productId}/styles`);
-  http.get(`http://localhost:3000/products/${productId}/related`);
-  // sleep(1);
+  const BASE_URL = 'http://localhost:3000'; // make sure this is not production
+  const productId = Math.floor(Math.random() * 999999) + 1
+
+  let responses = http.batch([
+    [
+      'GET',
+      `${BASE_URL}/products/${productId}`,
+      null,
+      { tags: { name: 'getProduct' } },
+    ],
+    [
+      'GET',
+      `${BASE_URL}/products/${productId}/styles`,
+      null,
+      { tags: { name: 'getStyles' } },
+    ],
+    [
+      'GET',
+      `${BASE_URL}/products/${productId}/related`,
+      null,
+      { tags: { name: 'getRelated' } },
+    ],
+  ]);
+
+  sleep(1);
 }
+
+// export default function () {
+//   const productId = Math.floor(Math.random() * 999999) + 1
+//   http.get(`http://localhost:3000/products/${productId}`);
+//   http.get(`http://localhost:3000/products/${productId}/styles`);
+//   http.get(`http://localhost:3000/products/${productId}/related`);
+//   // sleep(1);
+// }
